d1f03150ae3a2addacbb57038429b94f5007db95
Douglas Oliveira <douglinhas_uchiha@hotmail.com>
2016-05-10 22:15:41 -0300

    secondcommit

---
+++ list-of-names/js/app.js
@@ -0,0 +1,22 @@
+var names = ['Cazares', 'Paolo', 'Vardy'];
+
+function addAllNames() {
+    for (var i = 0; i < names.length; i++) {
+        var li = createListItem(names[i]);
+        addNameToList(li);
+    }
+}
+
+function addNameToList(li) {
+    var ul = document.getElementById('list-name');
+    ul.appendChild(li);
+}
+
+function createListItem(name) {
+    var li = document.createElement('li');
+    li.innerHTML = name;
+    return li;
+}
+
+//init
+addAllNames();
